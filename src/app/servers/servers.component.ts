36c445fcf740341b4acdb59fb970b587d1c6086f
Moises <moyayala12@gmail.com>
2018-05-30 16:37:13 -0700

    understanding angular components

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-servers', // element selector
+
+  // selector: '[app-servers]', // attribute selector
+
+  selector: '.app-servers', // class selector
+  // templateUrl: './servers.component.html',  // ExternalFile
+  template: `
+    <app-server></app-server
+    ><app-server></app-server>
+  `, // InternalTemplate
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
